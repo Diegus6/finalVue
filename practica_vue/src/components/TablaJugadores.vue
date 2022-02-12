5d1913e0b5481810b9073679a8f2b3173e7be68d
Diego <diego.porteros6@gmail.com>
2022-02-12 11:33:08 +0100

    realizado sumar goles a los jugadores

--- practica_vue/src/components/TablaJugadores.vue
+++ practica_vue/src/components/TablaJugadores.vue
@@ -1,5 +1,4 @@
 <template>
-
   <table class="table col-6 table-striped">
     <thead>
       <tr>
@@ -9,49 +8,73 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(jugador , index) in jugadores" :key="index">
-        <td>{{jugador.name}}</td>
-        <td>{{jugador.scores}}</td>
-        <td v-if="mostrarBotones"><input v-model="goles[jugador.id]" type="number"><button @click="sumarGoles(jugador.id)" class="sumar btn btn-success">Sumar</button></td>
-        <td v-if="mostrarBotones"><button @click="eliminarJugador(jugador.id)" class="btn btn-danger">Eliminar</button></td>
+      <tr v-for="(jugador, index) in jugadores" :key="index">
+        <td>{{ jugador.name }}</td>
+        <td>{{ jugador.scores }}</td>
+        <td v-if="mostrarBotones">
+          <input v-model="goles[jugador.id]" type="number" /><button
+            @click="sumarGoles(jugador.id)"
+            class="sumar btn btn-success"
+          >
+            Sumar
+          </button>
+        </td>
+        <td v-if="mostrarBotones">
+          <button @click="eliminarJugador(jugador.id)" class="btn btn-danger">
+            Eliminar
+          </button>
+        </td>
       </tr>
     </tbody>
   </table>
-
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-  name: 'TablaClasificacion',
-  props:["jugadores","mostrarBotones"],
+  name: "TablaClasificacion",
+  props: ["jugadores", "mostrarBotones"],
   data() {
-      return {
-       goles:[]
-      };
+    return {
+      goles: [],
+    };
   },
   methods: {
-    eliminarJugador(id){
+    eliminarJugador(id) {
       console.log(id);
-      axios.delete("http://localhost:3000/players/"+id);
+      axios.delete("http://localhost:3000/players/" + id);
     },
-    sumarGoles(id){
-      console.log(this.goles[id]);
-    }
+    sumarGoles(id) {
+      if (isNaN(this.goles[id]) || this.goles[id] <= 0) {
+        alert("Meta los goles por favor");
+      } else {
+        
+        let jugador;
+        for (let i = 0; i < this.jugadores.length; i++) {
+          if (this.jugadores[i].id == id) {
+            jugador = this.jugadores[i];
+          }
+        }
+        jugador.scores += this.goles[id];
+        axios.put("http://localhost:3000/players/"+id,jugador)
+        .catch(function(error){
+                
+          alert("Se ha producido un error al insertar los datos")
+        });
+      }
+    },
   },
-  created() {
-    console.log(this.jugadores);
-  },
-}
+  created() {},
+};
 </script>
 
 <style scoped>
-input[type=number]{
+input[type="number"] {
   width: 30%;
 }
 
-.sumar{
+.sumar {
   margin-left: 5%;
 }
 </style>
